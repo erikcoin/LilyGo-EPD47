bba21dddca58460c8467c7ad17097af3a659890e
erikcoin <erikdijkstra@hotmail.com>
2025-04-11 23:01:20 +0200

    Update utilities.h

--- src/utilities.h
+++ src/utilities.h
@@ -29,21 +29,21 @@
 
 #define BUTTON_1   (21)
 
-#define BATT_PIN   (14)
+#define BATT_PIN   (3)
 
-#define SD_MISO    (16)
-#define SD_MOSI    (15)
-#define SD_SCLK    (11)
-#define SD_CS      (42)
+#define SD_MISO    (40)
+#define SD_MOSI    (38)
+#define SD_SCLK    (39)
+#define SD_CS      (47)
 
-#define BOARD_SCL  (17)
-#define BOARD_SDA  (18)
-#define TOUCH_INT  (47)
+#define BOARD_SCL  (42)
+#define BOARD_SDA  (41)
+#define TOUCH_INT  (48)
 
-#define GPIO_MISO  (45)
-#define GPIO_MOSI  (10)
-#define GPIO_SCLK  (48)
-#define GPIO_CS    (39)
+#define GPIO_MISO  (40)
+#define GPIO_MOSI  (38)
+#define GPIO_SCLK  (39)
+#define GPIO_CS    (47)
 
 
 #endif
